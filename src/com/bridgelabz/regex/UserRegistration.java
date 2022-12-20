3d6dea100baab9604ca7769d92efa90aa22758d7
devraajmadhuri@gmail.com <devraajmadhuri@gmail.com>
2022-12-20 23:02:03 +0530

    Added:pattern to clear all email samples provided seperately

--- src/com/bridgelabz/regex/UserRegistration.java
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -1,12 +1,12 @@
 package com.bridgelabz.regex;
-
+import java.util.ArrayList;
 import java.util.Scanner;
-
 public class UserRegistration {
     public static void main (String[] args){
         System.out.println("User Registration Problem");
         Scanner scanner=new Scanner(System.in);
         UserRegistrationInput userRegistration=new UserRegistrationInput();
+        ArrayList<String> emails = new ArrayList<>();
 
         System.out.println("Enter first name:->");
         String firstName=scanner.next();
@@ -42,5 +42,43 @@ public class UserRegistration {
             System.out.println("Valid password");
         else
             System.out.println("Invalid password");
+
+        /**
+         * list of valid mail Id's
+          */
+        emails.add("abc@yahoo.com");
+        emails.add("abc-100@yahoo.com");
+        emails.add("abc.100@yahoo.com");
+        emails.add("abc111@abc.com");
+        emails.add("abc-100@abc.net");
+        emails.add("abc.100@abc.com.au");
+        emails.add("abc@1.com");
+        emails.add("abc@gmail.com.com");
+        emails.add("abc+100@gmail.com");
+
+        /**
+         * list of Invalid mail Id's
+         */
+        emails.add("abc");
+        emails.add("abc@.com.my");
+        emails.add("abc123@gmail.a");
+        emails.add("abc123@.com");
+        emails.add("abc123@.com.com");
+        emails.add(".abc@abc.com");
+        emails.add("abc()*@gmail.com");
+        emails.add("abc@%*.com");
+        emails.add("abc..2002@gmail.com");
+        emails.add("abc.@gmail.com");
+        emails.add("abc@abc@gmail.com");
+        emails.add("abc@gmail.com.1a");
+        emails.add("abc@gmail.com.aa.au");
+
+        for (String mail : emails){
+            if (userRegistration.isEmailValid(mail))
+                System.out.println(mail + " Valid Email");
+            else
+                System.out.println(mail + " Invalid Email");
+
+        }
     }
 }
